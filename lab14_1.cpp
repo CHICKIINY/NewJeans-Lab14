c9663b5bb75fb71ea31c2f4ce2cfc1dc5c94575e
CHICKIINY <bb.bright36389@gmail.com>
2023-01-13 17:36:03 +0700

    done

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,51 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[])
+{
+    double AM = 0;
+    for(int i = 0; i < N;i++)
+    {
+       AM += A[i];
+       B[0] = AM/N;
+
+    }
+    double SD = 0;
+    for(int i = 0; i < N;i++)
+    {
+        SD += pow(A[i],2);
+        B[1] = sqrt((SD/N)-pow(B[0],2));
+    }
+    double GM = 1;
+    for(int i = 0; i < N;i++)
+    {
+        GM *= A[i];
+        B[2] = pow(GM,1.0/N);
+    }
+    double HM = 0;
+    for(int i = 0; i < N;i++)
+    {
+        HM += 1/A[i];
+        B[3] = N/HM;
+    }
+    double max = A[0];
+    for(int i = 0; i < N;i++)
+    {
+        if(max < A[i])
+        {
+            max = A[i];
+            B[4] = max;
+        }
+    }
+    double min = A[0];
+    for(int i = 0; i < N;i++)
+    {
+        if(min > A[i])
+        {
+            min = A[i];
+            B[5] = min;
+        }
+    }
+
+}
